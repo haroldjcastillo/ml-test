cbe2e6da430cb27dbe607a9f850f6b4d969343fd
Harold Castillo <harold.j.castillo.o@hotmail.com>
2017-12-26 23:36:52 -0600

    Covariance, Correlation and Simple linear regression examples

---
+++ correlation.py
@@ -0,0 +1,47 @@
+# The formula for the correlation (r) is
+# r = (1/n-1) ( (∑x ∑y (x - ẍ) (y - ÿ) ) / Sx Sy)
+
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+data_set = [[1, 2, 4, 3, 5], [1, 3, 3, 2, 5]]
+# data_set = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [5, 4, 5, 6, 8, 9, 10, 13, 12]]
+
+print("X: ", data_set[0])
+print("Y: ", data_set[1])
+
+x_mean = np.mean(data_set[0])
+y_mean = np.mean(data_set[1])
+print("x mean: " + str(x_mean) + " y mean: " + str(y_mean))
+
+
+def std(data):
+    mean = np.mean(data)
+    return math.sqrt(np.sum(np.power(np.subtract(data, mean), 2)) / (len(data) - 1))
+
+
+def var(data):
+    return (np.sum(np.power(data, 2)) / len(data)) - math.pow(np.mean(data), 2)
+
+
+variance_x = var(data_set[0])
+variance_y = var(data_set[1])
+print("Variance x: " + str(variance_x) + " Variance y: " + str(variance_y))
+
+x_y = np.multiply(data_set[0], data_set[1])
+print("xy: ", x_y, " result: " + str(np.sum(x_y)))
+
+# Covariance
+# C(xy) = [(∑xy)/n] - (ẍ)(ÿ)
+covariance = (np.sum(x_y) / len(data_set[0])) - (x_mean * y_mean)
+print("Covariance: " + str(covariance))
+
+r = covariance / (math.sqrt(variance_x) * math.sqrt(variance_y))
+print("Correlation coeficient: " + str(r))
+
+# y = B0 + B1 * x
+# B1 = corr(x, y) * stdev(y) / stdev(x)
+
+B1 = r * std(data_set[1]) / std(data_set[0])
+print("Output value: " + str(B1))
--- simlinreg.py
+++ simlinreg.py
@@ -3,14 +3,10 @@
 #   y = B0 + B1 * x
 #   B1 = sum((xi-mean(x)) * (yi-mean(y))) / sum((xi – mean(x))^2)
 #   B0 = mean(y) – B1 * mean(x)
-# Root Mean Squared Error
-#   RMSE = sqrt( sum( (pi – yi)^2 )/n )
 
+import math
 import numpy as np
 from matplotlib import pyplot as plt
-
-# data_set = [[1.47, 1.50, 1.52, 1.55, 1.57, 1.60, 1.63, 1.65, 1.68, 1.70, 1.73, 1.75, 1.78, 1.80, 1.83],
-#            [52.21, 53.12, 54.48, 55.84, 57.20, 58.57, 59.93, 61.29, 63.11, 64.47, 66.28, 68.10, 69.92, 72.19, 74.46]]
 
 data_set = [[1, 2, 4, 3, 5], [1, 3, 3, 2, 5]]
 
@@ -33,12 +29,18 @@ for i in data_set[0]:
     y = b_0 + (b_1 * i)
     predict.append(round(y, 2))
 
-print(predict)
-
 plt.scatter(data_set[0], data_set[1], marker='o', c='b')
-plt.plot(predict, data_set[0])
+plt.plot(predict, data_set[0], '-bo')
 plt.title('Simple Linear Regression')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.grid(b=True, which='major', color='b', linestyle='-')
 plt.show()
+
+# Root Mean Squared Error
+#   RMSE = sqrt( sum( (pi – yi)^2 )/n )
+
+error = np.subtract(predict, data_set[1])
+squared_error = np.power(error, 2)
+rmse = math.sqrt(np.sum(squared_error) / len(data_set[1]))
+print(rmse)
